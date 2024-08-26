28271e30f405e72a3b672fdc05f0b18ed8c5801f
wujianwen <wujw58@126.com>
2024-08-26 15:18:07 +0800

    wasm build

--- index.cd5ba.js
+++ index.cd5ba.js
@@ -1,4 +1,4 @@
-System.register(["./application.js"], function (_export, _context) {
+System.register(["./application.bc9af.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
